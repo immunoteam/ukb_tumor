39bf49ba591d48b1e72c984c2a348a6b0e23433b
KBalazs1987 <koncz.balazs7@gmail.com>
2025-06-16 09:23:11 +0200

    asdf

--- scripts/B_005_PTV_burden_antigen_presentation.R
+++ scripts/B_005_PTV_burden_antigen_presentation.R
@@ -5,6 +5,7 @@ library("clusterProfiler")
 library("org.Hs.eg.db")
 
 ptvb_wMAF = fread("PTVvars/PTV_2024_05/MAF10_4_all_retained_variants_PTVBurden_final_Shetscores.tsv")
+ptvb_wMAF = fread("PTVvars/dl_20250331/MAF_10-3/PTVBurden_with_Shetscores.tsv")
 
 genes_all = data.frame(ENSEMBL = sort(unique(unlist(strsplit(ptvb_wMAF$Genes, ","), use.names = F))))
 gene_symbols = bitr(geneID = genes_all$ENSEMBL, fromType = "ENSEMBL", toType = "SYMBOL", OrgDb = "org.Hs.eg.db")
@@ -28,7 +29,7 @@ go_desc_list_ensg = lapply(go_desc_list, function(x) {
   g[!is.na(g)]
 })
 
-plan(multisession(workers = 4))
+plan(multisession(workers = 2))
 temp = future_sapply(ptvb_wMAF$Genes, function(x) {
   g = strsplit(x, ",")[[1]]
   future_sapply(go_desc_list_ensg, function(y) sum(g %fin% y))
@@ -48,5 +49,10 @@ ptvb_antproc$antigen_processing_all = unname(temp)
 
 apply(ptvb_antproc[,2:ncol(ptvb_antproc)], 2, Table)
 
-save(ptvb_antproc, file = "Objects/ptvb_antproc")
+save(ptvb_antproc, file = "Objects/ptvb_antproc_103")
+save(ptvb_antproc, file = "Objects/ptvb_antproc_104")
+
+load("Objects/ptvb_antproc_104")
+
+##MAF10-3
 
